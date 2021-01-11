b6b3f172b65774b74b7c04a551140c0cbf5e3b0c
aditineemkar <74664973+aditineemkar@users.noreply.github.com>
2021-01-11 11:24:49 +0530

    Update divisions.js

--- js/divisions.js
+++ js/divisions.js
@@ -1,18 +1,9 @@
-class Divisions {
-    constructor(x, y, w, h) {
-        var options = {
-
-            isStatic: true
-        }
-        this.body = Bodies.rectangle(x, y, w, h, options);
-        this.w = w;
-        this.h = h;
-        World.add(world, this.body);
-    }
-    display() {
-        var pos = this.body.position;
-        rectMode(CENTER);
-        fill("white");
-        rect(pos.x, pos.y, this.w, this.h);
-    }
-};
+class Divisions extends Ground {
+  display() {
+    var Color = color(random(0, 255), random(0, 255), random(0, 255));
+    var pos = this.body.position;
+    rectMode(CENTER);
+    fill(Color);
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+}
